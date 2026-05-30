a531cbec5931f66db3041d44dd489fd1efa97503
sahilaroora <sarora203@gmail.com>
2026-05-30 11:24:05 +0530

    React Router assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Navigation from "./Navigation";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navigation />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
