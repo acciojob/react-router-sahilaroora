ab91a387093fe3b95b5d929c4e46b6ac0b257770
sahilaroora <sarora203@gmail.com>
2026-05-30 13:08:03 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Navigation from "./Navigation";
-
+// import Navigation from "./Navigation";
+import Navigation from '../components/Navigation'
 function App() {
   return (
     <Router>
